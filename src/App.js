3c57d9199e2c5fc545b9ef73bf8d4eeab92089c6
Sachi <sachijoshi85320@gmail.com>
2022-11-06 16:18:30 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <li>About Us</li>
+        <li>me</li>
+        <li>to you to be</li>
+        <li>hiuihb</li>
+        <li>gyui</li>
+        <li>usgwudu</li>
+      </nav>
+      <div className="container"></div>
+    </div>
+  );
+}
+
+export default App;
